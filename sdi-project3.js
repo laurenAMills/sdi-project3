9b97dfb68d7178deb47e11b1211c17b23d182be8
laurenAMills <laurenmills@laurens-mbp.attlocal.net>
2014-07-23 23:16:40 -0700

    First push to GitHub.

---
+++ sdi-project3.js
@@ -0,0 +1,16 @@
+alert("JavaScript works!");
+//Lauren Mills
+//SDI-Project 3
+//SDI 1407
+
+//Global Variables
+var me = "Lauren";
+var myBoss = "Yves";
+var workHoursADay = 16;
+var topicsOfTheDay = ["The 7D Mine Train App", "Frozen Karaoke App Localization", "Olaf App", "Disney Villains App", "Miles From Tomorrowland App"]
+var HoursSpentOnProjects = [2, 5, 3, 6]
+
+//Prompt
+console.log ("My Boss, " + myBoss + ", and I, " + me + ", are going to a very important meeting.  We need to figure out which project is going to excite the audience the most.");
+
+var whatAreWeGoingToDo = ()
