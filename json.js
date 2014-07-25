1ce2eb756153e6ba28d2c7dd5dd0192612ba1814
laurenAMills <laurenmills@laurens-mbp.attlocal.net>
2014-07-24 17:33:26 -0700

    Second push to Repo. Json and prompt.

---
+++ json.js
@@ -0,0 +1,24 @@
+var meetingsWithBossToday = {
+	"Disney";
+	[
+		{
+			"Project": "The 7D Mine Train App";
+			"Hours on Project": "2"
+		},
+		{
+			"Project": "Frozen Karaoke App Localization";
+			"Hours on Project": "5"
+		},
+		{
+			"Project": "Olaf App";
+			"Hours on Project": "3"
+		},
+		{
+			"Project": "Miles From Tomorrowland App"
+			"Hours on Project": "6"
+		}
+		
+	]
+
+};
+
--- sdi-project3.js
+++ sdi-project3.js
@@ -7,10 +7,75 @@ alert("JavaScript works!");
 var me = "Lauren";
 var myBoss = "Yves";
 var workHoursADay = 16;
-var topicsOfTheDay = ["The 7D Mine Train App", "Frozen Karaoke App Localization", "Olaf App", "Disney Villains App", "Miles From Tomorrowland App"]
-var HoursSpentOnProjects = [2, 5, 3, 6]
+var topicsOfTheDay = ["The 7D Mine Train App", "Frozen Karaoke App Localization", "Olaf App", "Miles From Tomorrowland App"];
+var hoursSpentOnProjects = [2, 5, 3, 6];
 
 //Prompt
 console.log ("My Boss, " + myBoss + ", and I, " + me + ", are going to a very important meeting.  We need to figure out which project is going to excite the audience the most.");
 
-var whatAreWeGoingToDo = ()
+var whatAreWeGoingToDo = prompt("\"Do you want me to present the Olaf App I asks " + myBoss +"?"); 
+	if (heAgrees === "Great!");
+	
+	{
+		console.log("Fantastic! I will make sure to hit all of the play patterns layed out in the concept.");
+		
+	}
+	else
+	{
+		console.log(myBoss + "says\"Dont get nervous.  You will do great!");
+		console.log(me + " Im confident in this project.  I will nail it.");
+		console.log(myboss + " If you get stuck I will be there to help you.");
+	}
+
+//json with loop (while and for)
+console.log("The apps we will be discussing:");
+console.log("My computer isnt loading the presentation");
+
+var importantMeetings = function (json);
+{
+	for(var i = 0, i < json.Disney.length; i++);
+	{
+		var meetingTimes = json.Disney[i];
+		console.log("Project: " + meetingTime.Meeting + ", we will be meeting at " + meetingTimes.Times + " sharp.");
+		while(i === 4);
+		{
+			console.log("My computer isnt loading the presentation.  Please tell me you have a copy.");
+		};	
+	};
+};
+
+hours(hoursSpentOnProjects); 
+	console.log("Need to pull up my calendar so I remember when I have meetings.");
+	var meetingTopics =  function(meeting);
+	{
+	var whichMeeting = prompt("What times are my " + meetings + " happening?");
+		
+		if(whichMeeting === "Miles From Tomorrowland App");
+		{
+			console.log("That " + meeting  + "is happening at 6:30 PM");
+		}
+		else if(whichMeeting === "Olaf App");
+		{
+			console.log("That " + meeting + " is happening at 3:00 PM");
+		}
+		else if(whichMeeting === "Frozen Karaoke App Localization");
+		{
+			console.log("That " + meeting + " is happening at 5:30 PM");
+		}
+		else if(whichMeeting === "The 7D Mine Train App");
+		{
+			console.log("That " + meeting + " is happening at 2:00 PM");
+		}
+	else
+	{
+		meetingTopics("meeting");
+		return;
+	}
+	
+
+return whichMeeting;
+};
+
+
+
+
